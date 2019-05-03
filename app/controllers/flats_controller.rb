378b33f21a989db7d4cd3f5791b937d018e60556
Dang-Thanh Tran <trandgthanh@gmail.com>
2019-05-03 11:15:03 +0200

    done maps

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,11 +1,36 @@
 class FlatsController < ApplicationController
   before_action :set_flat, only: [:show, :edit, :update, :destroy]
 
-  # GET /flats
-  # GET /flats.json
+
   def index
-    @flats = Flat.all
+    @flats = Flat.where.not(latitude: nil, longitude: nil)
+
+    @markers = @flats.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude
+      }
+    end
+
   end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   # GET /flats/1
   # GET /flats/1.json
